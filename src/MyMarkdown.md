21b2eb5021ac6ef7e37f580260bf00335a6059bd
Bohdan Leonets <bohdan@mytowerapp.com>
2022-10-01 21:13:45 +0300

    add vite plugin for md files

---
+++ src/MyMarkdown.md
@@ -0,0 +1,7 @@
+# Hello World
+
+This is some markdown
+
+* Item 1
+* Item 2
+* Item 3
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import md from "./vite-plugin-vue-md.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    md({
+      wrapperElement: "section",
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
